336455a4c9042c1eb08bd825f7d807c00dd5a1f9
Konstantyn <k.buhantsev@gmail.com>
2022-10-17 13:59:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-// import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { StyledEngineProvider } from '@mui/material/styles';
+import {
+  StyledEngineProvider,
+  ThemeProvider,
+  createTheme,
+} from '@mui/material/styles';
 
-// const theme = createTheme();
+const theme = createTheme();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
-      {/* <ThemeProvider theme={theme}> */}
-
-      <App />
-      {/* </ThemeProvider> */}
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </StyledEngineProvider>
   </React.StrictMode>
 );
